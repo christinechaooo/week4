eb2a3d4dd4d8d3352c86053ff23d040db44ce786
Christine Chao <christinechaooo@gmail.com>
2014-07-01 07:10:19 -0700

    Initial Commit

---
+++ week4/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  week4
+//
+//  Created by Christine Chao on 7/1/14.
+//  Copyright (c) 2014 Christine Chao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
